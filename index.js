f1e34da9e697ffe2b27a0c34363b794773ddf653
paveshkanungo <paveshkanungo@gmail.com>
2023-11-11 19:23:46 +0530

    new commit

--- index.js
+++ index.js
@@ -44,7 +44,7 @@
 
 const userTab = document.querySelector("[data-userWeather]");
 const searchTab = document.querySelector("[data-searchWeather]");
-const userContainer = document.querySelector(".weather-conatiner");
+const userContainer = document.querySelector(".weather-container");
 
 const grantAccessContainer = document.querySelector(".grant-location-container");
 const searchForm = document.querySelector("[data-searchForm]");
